f7a58adfb2e4b44a4e06fede420a7ae15bf918d1
Elad Levy <eladlevy104@outlook.com>
2025-01-21 14:20:22 +0200

    secend  commit

---
+++ Main.py
@@ -0,0 +1,19 @@
+x = 5
+y = 5
+list_of_numbers = [3, 6, 9, 12]
+
+def sum_number():
+    print(x + y)
+
+def count_numbers():
+    print(len(list_of_numbers))
+
+def Print_text(text):
+    print(f"hello, {text}")
+
+def calculate(num1, num2):
+   print("getting numbers")
+   result = num1 + num2
+   print("calculate result")
+   print(f"{result}")
+
---
+++ caliing_to_function.py
@@ -0,0 +1,10 @@
+import Main
+
+Main.sum_number()
+
+Main.count_numbers()
+
+Main.Print_text("Alex")
+Main.Print_text("Elad")
+
+Main.calculate(5,1)
